b1aa1830c9a443d8263aa4d67d6963fddc170582
saifullah199 <mdkhalidsaifullah199@gmail.com>
2025-11-18 16:56:17 +0600

    add id to localStorage

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
+import { addToStoredDB } from '../../utility/addToDB';
 
 const BookDetails = () => {
     const {id} = useParams()
@@ -7,15 +8,21 @@ const BookDetails = () => {
     const data = useLoaderData()
     const singleBook = data.find(book => book.bookId === bookId)
     console.log(singleBook)
-    const {bookName,image, 
-} =singleBook
+    const {bookName,image} =singleBook
 
+    const handleMarkAsRead = id => {
+
+        addToStoredDB(id)
+
+    }
     return (
         <div className='w-2/3 mx-auto'>
             <img className='w-48' src={image} alt="" />
             <h4>{bookName}</h4>
-            <button className='btn btn-accent m-2'> Read</button>
-            <button className='btn btn-info m-2'> WishList</button>
+            <button 
+            onClick={() => handleMarkAsRead(id)} 
+            className='btn btn-accent m-2'>Mark as Read</button>
+            <button className='btn btn-info m-2'> Add to WishList</button>
         </div>
     );
 };
---
+++ src/utility/addToDB.js
@@ -0,0 +1,26 @@
+const getStoredBook =() => {
+    const storedBookSTR  = localStorage.getItem('readlist');
+
+    if(storedBookSTR){
+        const storedBookData = JSON.parse(storedBookSTR)
+        return storedBookData
+    }
+    else{
+        return [];
+    }
+}
+
+const addToStoredDB = (id) => {
+    const storedBookData = getStoredBook();
+
+    if(storedBookData.includes(id)){
+        alert('id already exist')
+    }
+    else{
+        storedBookData.push(id);
+       const data = JSON.stringify(storedBookData)
+       localStorage.setItem("readlist", data)
+    }
+}
+
+export {addToStoredDB}
